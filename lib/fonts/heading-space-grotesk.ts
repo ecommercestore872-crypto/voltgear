5b2164f477c55224b118bff56ff915611a2dbd4b
Malik Salman Tanveer <maliksalmantanveer@gmail.com>
2026-09-22 10:18:48 +0500

    fix(build): Space Grotesk variable font for Vercel next/font loader

--- lib/fonts/heading-space-grotesk.ts
+++ lib/fonts/heading-space-grotesk.ts
@@ -2,7 +2,6 @@ import { Space_Grotesk } from "next/font/google";
 
 export const headingFont = Space_Grotesk({
   subsets: ["latin"],
-  weight: ["500", "600", "700"],
   variable: "--font-heading",
   display: "swap",
   preload: false,
